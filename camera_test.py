e5abff4cbb8be7fe92e500cfb4b87223e40eaa82
Michael Sherman <shermanm@uchicago.edu>
2025-07-31 19:15:48 -0500

    fixup

--- camera_test.py
+++ camera_test.py
@@ -4,10 +4,13 @@ from picamera2 import Picamera2
 
 picam2 = Picamera2()
 
+camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)})
+picam2.configure(camera_config)
+
 picam2.start()
 time.sleep(2)
 
-metadata = picam2.capture_file("/app/test2.jpg")
+metadata = picam2.capture_file("/app/test2.png")
 print(metadata)
 
 picam2.close()
